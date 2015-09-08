8a0bd04085feb1007146d3037fd358b28f4a9467
yschutz <schutz@in2p3.fr>
2015-09-08 18:33:28 +0200

    to make git happy

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@
 #include "cubemesh.h"
 #include "modelmesh.h"
 #include "annulusmesh.h"
-#include "tube.h"
+#include "vertex.h"
+
 #include <QVector2D>
 using namespace cgl;
 
@@ -15,24 +16,25 @@ int main(int argc, char *argv[])
     QGuiApplication a(argc, argv);
 
 // --- METHODE FACILE ------
-        View * view = new View();
+//        View * view = new View();
 
 
-      ModelMesh * mesh = new ModelMesh(":/man.obj");
+//      AnnulusMesh * mesh = new AnnulusMesh(5.0, 4.0, 4);
 
-        mesh->setTextureImage(":/textures/damier.png");
-        view->scene()->addMesh(mesh);
-        view->show();
-
-        view->scene()->setDebug(true);
+//        mesh->setTextureImage(":/textures/images/BlueQuark.png");
+//        view->scene()->addMesh(mesh);
+//        view->show();
+//        for (int index = 0; index < mesh->verticesCount(); index++)
+//            qDebug() << mesh->vertex(index).x() << mesh->vertex(index).y() << mesh->vertex(index).z() << mesh->vertex(index).u() << mesh->vertex(index).v();
+//        view->scene()->setDebug(true);
 // -------------------------
 
 
  //---- METHODE ON AIME SE PRENDRE LA TETE
-//    LaboratoryView w;
+    LaboratoryView w;
 //    w.scene()->setDebug(false);
 
-//    w.show();
+    w.show();
 // -------------------------
 
 
